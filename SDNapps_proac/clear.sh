1152c3518fe9075609dddb9a12baf2309c7b3a15
andy010645 <69691891+andy010645@users.noreply.github.com>
2021-11-01 18:04:28 +0800

    Add files via upload

---
+++ SDNapps_proac/clear.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo rm times.txt
+#cd ~/ryu/ryu/app/SDNapps_proac/Metrics/
+cd Metrics
+sudo rm *metrics*
+cd ..
+#cd ~/ryu/ryu/app/SDNapps_proac/RoutingGeant/stretch
+cd RoutingGeant/stretch
+sudo rm *stretch*
+
+
